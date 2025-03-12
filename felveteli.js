03738cc9f9b97b732f3d4cd7ff96539fbca449a8
kabzso820 <kabzso820@hengersor.hu>
2025-03-12 11:29:20 +0100

    lekerdezes

--- felveteli.js
+++ felveteli.js
@@ -20,6 +20,14 @@ app.get("/", (req, res) => {
     res.send("Működik a szerver.")
 })
 
+app.get("/v", (req,res) => {
+    const sql = "SELECT *FROM felvitelizok";
+    db.query(sql , (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    })
+})
+
 app.listen(3000, () =>{
     console.log("A felveteli szervere a 3000-es porton fut");
 })
